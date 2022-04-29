daa8c389c3aa5e138c9425a4272770ce6ccab027
Krzyszto Niemiec <krzysztof.niemiec@gmail.com>
2022-04-29 12:35:52 +0200

    Test fixed but disabled.

--- src/test/java/com/kniemiec/soft/transferorchestrator/compliance/DefaultComplianceCheckClientTest.java
+++ src/test/java/com/kniemiec/soft/transferorchestrator/compliance/DefaultComplianceCheckClientTest.java
@@ -1,63 +1,46 @@
 package com.kniemiec.soft.transferorchestrator.compliance;
 
-import com.kniemiec.soft.compliance.ComplianceCheckGrpc;
-import com.kniemiec.soft.compliance.ComplianceCheckResponse;
 import com.kniemiec.soft.transferorchestrator.MockData;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import org.grpcmock.springboot.AutoConfigureGrpcMock;
-import org.junit.jupiter.api.AfterEach;
+
+import com.kniemiec.soft.transferorchestrator.transfer.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Optional;
+import static org.mockito.Mockito.verify;
 
-import static org.grpcmock.GrpcMock.*;
-
-
-@DirtiesContext
-@SpringBootTest
-@AutoConfigureGrpcMock
-@ActiveProfiles("test")
 class DefaultComplianceCheckClientTest {
 
-    @Value("${grpcmock.server.port}")
-    private int grpcMockPort;
 
-//    private ManagedChannel managedChannel;
-
-    @Autowired
     private DefaultComplianceCheckClient complianceCheckClient;
+
+    @Mock
+    WebClient webClient;
 
     @BeforeEach
     void setUp(){
+        webClient = Mockito.mock(WebClient.class);
+        complianceCheckClient = new DefaultComplianceCheckClient(webClient);
     }
-//
-//    @AfterEach
-//    void shutdownChannel() {
-//        Optional.ofNullable(managedChannel).ifPresent(ManagedChannel::shutdownNow);
-//    }
 
 
     @Test
-    @Disabled
     void check() {
         // given
-        stubFor(unaryMethod(ComplianceCheckGrpc.getCheckComplianceMethod())
-                .willReturn(ComplianceCheckResponse.newBuilder().build()));
-
 
         // when
         complianceCheckClient.check("transferId", MockData.mockSenderUserData(), MockData.mockRecipientUserData() );
 
         // then
-        verifyThat(calledMethod(ComplianceCheckGrpc.getCheckComplianceMethod()));
+        verify(webClient.put());
 
     }
 }
