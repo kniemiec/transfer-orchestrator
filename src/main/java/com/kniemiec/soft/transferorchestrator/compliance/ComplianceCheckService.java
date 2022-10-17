f33add5b949c70b84e8f5b01c45125fbdcabda9d
Krzysztof Niemiec <krzysztof.niemiec@pi-software.com>
2022-10-17 13:37:06 +0200

    Migration to kubernetes completed for transfer orchestrator and payout.

--- src/main/java/com/kniemiec/soft/transferorchestrator/compliance/ComplianceCheckService.java
+++ src/main/java/com/kniemiec/soft/transferorchestrator/compliance/ComplianceCheckService.java
@@ -1,6 +1,5 @@
 package com.kniemiec.soft.transferorchestrator.compliance;
 
-import com.kniemiec.soft.transferorchestrator.compliance.model.ComplianceResponse;
 import com.kniemiec.soft.transferorchestrator.transfer.model.User;
 import reactor.core.publisher.Mono;
 
