e77b399e7f5af3926edb179881f4675f46bff1ed
Krzysztof Niemiec <krzysztof.niemiec@gmail.com>
2023-04-05 11:10:38 +0200

    Firt usage of copilot. Test implemented, not very clever but pass.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Fictional transfer orchestrator
 TBD
 
 
-## ChangeLog
-
-##### 13.12.2022: OpenAPI support added
+## ChangeLog:
+###### 05.04.2022: Test added by Copitol.
+###### 14.12.2022: Telemetry&Jaeger support added
+###### 13.12.2022: OpenAPI support added
---
+++ src/test/java/com/kniemiec/soft/transferorchestrator/confirmation/TopUpConfirmationServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.kniemiec.soft.transferorchestrator.confirmation;
+
+// generate unit tests for class com.kniemiec.soft.transferorchestrator.confirmation.TopUpConfirmationServiceImpl
+// using JUnit 4
+
+import com.kniemiec.soft.transferorchestrator.topup.TopUpConfirmationServiceGrpc;
+import com.kniemiec.soft.transferorchestrator.topup.TopUpRequest;
+import com.kniemiec.soft.transferorchestrator.topup.TopUpResponse;
+import com.kniemiec.soft.transferorchestrator.transfer.persistence.DataTransferRepository;
+import com.kniemiec.soft.transferorchestrator.transfer.TransferProcessor;
+import com.kniemiec.soft.transferorchestrator.transfer.model.Status;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class TopUpConfirmationServiceImplTest {
+
+    @Mock
+    private TransferProcessor transferProcessor;
+
+    @Mock
+    private DataTransferRepository dataTransferRepository;
+
+    @InjectMocks
+    private TopUpConfirmationServiceImpl topUpConfirmationService;
+
+    @Test
+    public void topUpCompleted() {
+        TopUpRequest request = TopUpRequest.newBuilder().setTransferId("1").build();
+        TopUpResponse response = TopUpResponse.newBuilder().setTransferId("1").build();
+        when(dataTransferRepository.findById(anyString())).thenReturn(Mono.just(new com.kniemiec.soft.transferorchestrator.transfer.model.TransferData()));
+        when(dataTransferRepository.save(any())).thenReturn(Mono.just(new com.kniemiec.soft.transferorchestrator.transfer.model.TransferData()));
+        when(transferProcessor.addToQueue(any())).thenReturn(new com.kniemiec.soft.transferorchestrator.transfer.model.TransferData());
+        topUpConfirmationService.topUpCompleted(request, new StreamObserver<TopUpResponse>() {
+            @Override
+            public void onNext(TopUpResponse value) {
+                assert value.getTransferId().equals("1");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        });
+
+        verify(dataTransferRepository, times(1)).findById(anyString());
+        verify(dataTransferRepository, times(1)).save(any());
+        verify(transferProcessor, times(1)).addToQueue(any());
+    }
+}
